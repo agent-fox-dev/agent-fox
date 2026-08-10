6f97d514e83c9a68ac73196e58caca5c58ddd545
Michael Kuehl <hello@micku.me>
2026-08-10 09:20:40 +0200

    removed spec from the installer

--- install.sh
+++ install.sh
@@ -3,5 +3,4 @@
 set -eu
 
 uv tool install af --from git+https://github.com/agent-fox-dev/agent-fox.git#subdirectory=packages/af
-uv tool install spec --from git+https://github.com/agent-fox-dev/agent-fox.git#subdirectory=packages/spec
 uv tool install nightshift --from git+https://github.com/agent-fox-dev/agent-fox.git#subdirectory=packages/nightshift
