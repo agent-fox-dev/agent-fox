4ebbab9d6d5dc77699fe1b695c84505f5b8efb9c
Michael Kuehl <hello@micku.me>
2026-09-01 18:30:21 +0200

    updated the README

--- install.sh
+++ install.sh
@@ -3,4 +3,3 @@
 set -eu
 
 uv tool install af --from git+https://github.com/agent-fox-dev/agent-fox.git#subdirectory=packages/af
-uv tool install nightshift --from git+https://github.com/agent-fox-dev/agent-fox.git#subdirectory=packages/nightshift
